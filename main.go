95a738c925a3016ed90506258d4a2a310b176a42
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-08-17 09:50:00 +0900

    init game

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	ScreenWidth  = 1920
+	ScreenHeight = 1080
+	boidCount    = 100
+)
+
+type Game struct{}
+
+func NewGame() (*Game, error) {
+	g := &Game{}
+
+	return g, nil
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return ScreenWidth, ScreenHeight
+}
+
+func main() {
+	game, err := NewGame()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
+	ebiten.SetFullscreen(true)
+	ebiten.SetWindowTitle("chatgpt")
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
