6ba21a9908c7c8141a0cd035b3713837987f9224
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-08-19 15:08:29 +0900

    refactor: split in files

---
+++ game.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	ScreenWidth  = 1920
+	ScreenHeight = 1080
+)
+
+type Game struct {
+	input *Input
+}
+
+func NewGame() (*Game, error) {
+	i, err := NewInput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	g := &Game{
+		input: i,
+	}
+	return g, nil
+}
+
+func (g *Game) Update() error {
+	g.input.Update()
+
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.input.Draw(screen)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return ScreenWidth, ScreenHeight
+}
---
+++ input.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
+
+type Input struct {
+	runes   []rune
+	text    string
+	counter int
+}
+
+func NewInput() (*Input, error) {
+	i := &Input{
+		text:    "Type on the keyboard:\n",
+		counter: 0,
+	}
+	return i, nil
+}
+
+func (i *Input) Update() error {
+	// Add runes that are input by the user by AppendInputChars.
+	// Note that AppendInputChars result changes every frame, so you need to call this
+	// every frame.
+	i.runes = ebiten.AppendInputChars(i.runes[:0])
+	i.text += string(i.runes)
+
+	// Adjust the string to be at most 10 lines.
+	ss := strings.Split(i.text, "\n")
+	if len(ss) > 10 {
+		i.text = strings.Join(ss[len(ss)-10:], "\n")
+	}
+
+	// If the enter key is pressed, add a line break.
+	if repeatingKeyPressed(ebiten.KeyEnter) || repeatingKeyPressed(ebiten.KeyNumpadEnter) {
+		i.text += "\n"
+	}
+
+	// If the backspace key is pressed, remove one character.
+	if repeatingKeyPressed(ebiten.KeyBackspace) {
+		if len(i.text) >= 1 {
+			i.text = i.text[:len(i.text)-1]
+		}
+	}
+
+	i.counter++
+	return nil
+}
+
+func (i *Input) Draw(screen *ebiten.Image) {
+	// Blink the cursor.
+	t := i.text
+	if i.counter%60 < 30 {
+		t += "_"
+	}
+	ebitenutil.DebugPrint(screen, t)
+}
+
+// repeatingKeyPressed return true when key is pressed considering the repeat state.
+func repeatingKeyPressed(key ebiten.Key) bool {
+	const (
+		delay    = 30
+		interval = 3
+	)
+	d := inpututil.KeyPressDuration(key)
+	if d == 1 {
+		return true
+	}
+	if d >= delay && (d-delay)%interval == 0 {
+		return true
+	}
+	return false
+}
