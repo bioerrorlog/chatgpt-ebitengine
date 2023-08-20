fd2225586e63432c72b5bfe5923faade1a2991e1
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-08-20 11:39:06 +0900

    add button

---
+++ button.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Button struct {
+	x, y          int
+	width, height int
+	isClicked     bool
+}
+
+func NewButton(x, y, width, height int) (*Button, error) {
+	b := &Button{
+		x:      x,
+		y:      y,
+		width:  width,
+		height: height,
+	}
+	return b, nil
+}
+
+func (b *Button) Update() error {
+	x, y := ebiten.CursorPosition()
+
+	// Check the cursor position
+	if x >= b.x && x <= b.x+b.width && y >= b.y && y <= b.y+b.height {
+		if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+			b.isClicked = true
+		} else {
+			b.isClicked = false
+		}
+	} else {
+		b.isClicked = false
+	}
+
+	return nil
+}
+
+func (b *Button) Draw(screen *ebiten.Image) {
+	buttonImage := ebiten.NewImage(b.width, b.height)
+	buttonColor := color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+	buttonImage.Fill(buttonColor)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(b.x), float64(b.y))
+	screen.DrawImage(buttonImage, op)
+}
+
+func (b *Button) IsClicked() bool {
+	return b.isClicked
+}
--- game.go
+++ game.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -12,7 +13,8 @@ const (
 )
 
 type Game struct {
-	input *Input
+	input      *Input
+	sendButton *Button
 }
 
 func NewGame() (*Game, error) {
@@ -21,20 +23,32 @@ func NewGame() (*Game, error) {
 		log.Fatal(err)
 	}
 
+	b, err := NewButton(500, 400, 100, 50)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	g := &Game{
-		input: i,
+		input:      i,
+		sendButton: b,
 	}
 	return g, nil
 }
 
 func (g *Game) Update() error {
 	g.input.Update()
+	g.sendButton.Update()
+
+	if g.sendButton.IsClicked() {
+		fmt.Println("Button was clicked!")
+	}
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.input.Draw(screen)
+	g.sendButton.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
