c382846c3392902786eb6276322e17f8b2d8383f
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-08-22 22:16:19 +0900

    change button color while pressed

--- game/button.go
+++ game/button.go
@@ -12,18 +12,20 @@ type Button struct {
 	x, y          int
 	width, height int
 	color         color.RGBA
+	colorClicked  color.RGBA
 	buttonText    string
 	isClicked     bool
 }
 
 func NewButton(x, y, width, height int, buttonText string) (*Button, error) {
 	b := &Button{
-		x:          x,
-		y:          y,
-		width:      width,
-		height:     height,
-		buttonText: buttonText,
-		color:      color.RGBA{R: 171, G: 104, B: 255, A: 255},
+		x:            x,
+		y:            y,
+		width:        width,
+		height:       height,
+		buttonText:   buttonText,
+		color:        color.RGBA{R: 171, G: 104, B: 255, A: 255},
+		colorClicked: color.RGBA{R: 171 - 20, G: 104 - 20, B: 255 - 20, A: 255},
 	}
 	return b, nil
 }
@@ -46,8 +48,13 @@ func (b *Button) Update() error {
 }
 
 func (b *Button) Draw(screen *ebiten.Image) {
+	fillColor := b.color
+	if b.isClicked {
+		fillColor = b.colorClicked
+	}
+
 	buttonImage := ebiten.NewImage(b.width, b.height)
-	buttonImage.Fill(b.color)
+	buttonImage.Fill(fillColor)
 	op := &ebiten.DrawImageOptions{}
 	op.GeoM.Translate(float64(b.x), float64(b.y))
 	screen.DrawImage(buttonImage, op)
